a067cda91b8390d1a5d915845709dfafe72d2c9f
rogec540 <rogec540@newschool.edu>
2024-04-08 12:47:31 -0400

    Create sketch.js

---
+++ src/js/sketch.js
@@ -0,0 +1,13 @@
+// GameB from Team One - Charlotte, Chloe, and Emma
+
+function preload() {
+    partyConnect("wss://demoserver.p5party.org", "team1_gameB");
+}
+
+function setup() {
+    createCanvas(640, 640);
+}
+
+function draw() {
+    background("gray");
+}
