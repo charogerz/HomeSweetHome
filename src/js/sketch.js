cfc2debab4bd195c573bfdf29d16300c6d447393
chloe yiu <chloeyiu@icloud.com>
2024-04-09 19:50:58 -0400

    enlarge canvas and reposition right shape

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -3,32 +3,32 @@
 let shapes = [];
 
 function preload() {
-    partyConnect("wss://demoserver.p5party.org", "team1_gameB");
+	partyConnect("wss://demoserver.p5party.org", "team1_gameB");
 }
 
 function setup() {
-    createCanvas(500, 500);
+	createCanvas(800, 800);
 }
 
 function draw() {
-    background("#f2f2f2");
+	background("#f2f2f2");
 
-    // screen divider
-    line(width/2, 0, width/2, height);
+	// screen divider
+	line(width / 2, 0, width / 2, height);
 
-    // left shape
-    fill("red");
-    ellipse(width/4, height/2, 100);
+	// left shape
+	fill("red");
+	ellipse(width / 4, height / 2, 100);
 
-    // right shape
-    fill("blue");
-    ellipse(width-125, height/2, 100);
+	// right shape
+	fill("blue");
+	ellipse(width - 200, height / 2, 100);
 
-    if(mouseIsPressed) {
-        push();
-        fill("#f2f2f2");
-        noStroke();
-        ellipse(mouseX, mouseY, 50);
-        pop();
-    }
+	if (mouseIsPressed) {
+		push();
+		fill("#f2f2f2");
+		noStroke();
+		ellipse(mouseX, mouseY, 50);
+		pop();
+	}
 }
