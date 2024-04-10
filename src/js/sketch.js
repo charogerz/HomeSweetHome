04591d5cadc76e27d16d17e82625dada353ea64d
rogec540 <rogec540@newschool.edu>
2024-04-09 22:44:21 -0400

    erasing!

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -1,20 +1,29 @@
 // GameB from Team One - Charlotte, Chloe, and Emma
 
-let shapes = [];
-
 function preload() {
-	partyConnect("wss://demoserver.p5party.org", "team1_gameB");
+	partyConnect("wss://demoserver.p5party.org", "team1_gameB"); 
 }
 
 function setup() {
 	createCanvas(800, 800);
+    drawShapes();
 }
 
 function draw() {
-	background("#f2f2f2");
+	if (mouseIsPressed) {
+		push();
+		fill("#f2f2f2");
+		noStroke();
+		ellipse(mouseX, mouseY, 50);
+		pop();
+	}
 
-	// screen divider
+    // screen divider
 	line(width / 2, 0, width / 2, height);
+}
+
+function drawShapes() {
+    background("#f2f2f2");
 
 	// left shape
 	fill("red");
@@ -23,12 +32,12 @@ function draw() {
 	// right shape
 	fill("blue");
 	ellipse(width - 200, height / 2, 100);
+}
 
-	if (mouseIsPressed) {
-		push();
-		fill("#f2f2f2");
-		noStroke();
-		ellipse(mouseX, mouseY, 50);
-		pop();
-	}
+function mouseDragged() {
+    push();
+    noStroke();
+    fill("#f2f2f2");
+    ellipse(mouseX, mouseY, 50);
+    pop();
 }
